4975f41b6faa1fd30c8559d9790976f1086c8ba6
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-12-22 10:41:01 -0500

    Create main.cpp

---
+++ programs/helper/main.cpp
@@ -0,0 +1,41 @@
+//=======================================================================
+// Copyright Avery Stafford 2020-2021.
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://www.opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <tlib/system.hpp>
+#include <tlib/errors.hpp>
+#include <tlib/print.hpp>
+
+tlib::print_line("Ardeny Foundation presents...");
+tlib::print_line("Arden Ware OS -Thorium-");
+tlib::print_line("---HELP---");
+tlib::print_line("Enter commands into the terminal. Some commands include:");
+tlib::print_line("alpha");
+tlib::print_line("args");
+tlib::print_line("bench");
+tlib::print_line("cat");
+tlib::print_line("cpuid");
+tlib::print_line("date");
+tlib::print_line("dctor");
+tlib::print_line("df");
+tlib::print_line("divzero");
+tlib::print_line("free");
+tlib::print_line("ifconfig");
+tlib::print_line("keyboard");
+tlib::print_line("long");
+tlib::print_line("longone");
+tlib::print_line("longtwo");
+tlib::print_line("loop");
+tlib::print_line("ls");
+tlib::print_line("lse820");
+tlib::print_line("mount");
+tlib::print_line("mkdir");
+tlib::print_line("mkfs");
+tlib::print_line("ps");
+tlib::print_line("reboot");
+tlib::print_line("shutdown");
+tlib::print_line("rm");
+tlib::print_line("wget");
